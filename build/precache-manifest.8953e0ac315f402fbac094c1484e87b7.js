2c33f4e9318a905b2fb3d69368748b33da8870fc
Jacob Moller <jacob.moller8@hotmail.com>
2019-03-01 17:30:12 +0100

    new files

---
+++ build/precache-manifest.8953e0ac315f402fbac094c1484e87b7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "edcc9969bee5c447f2b9",
+    "url": "/static/css/main.4f1258f7.chunk.css"
+  },
+  {
+    "revision": "edcc9969bee5c447f2b9",
+    "url": "/static/js/main.edcc9969.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d52635dfe3bc1160bc95",
+    "url": "/static/css/2.708a4351.chunk.css"
+  },
+  {
+    "revision": "d52635dfe3bc1160bc95",
+    "url": "/static/js/2.d52635df.chunk.js"
+  },
+  {
+    "revision": "4d5fb30feede3631487cb8816a3cccab",
+    "url": "/static/media/shoppingCart.4d5fb30f.svg"
+  },
+  {
+    "revision": "2e6aba66d28a0cbc85234639c4b338b1",
+    "url": "/static/media/delete.2e6aba66.svg"
+  },
+  {
+    "revision": "1a5ab88352089ca7e183c126ee5eb98e",
+    "url": "/static/media/Inventory.1a5ab883.svg"
+  },
+  {
+    "revision": "54a0c00875d6074e7e15d16beb860283",
+    "url": "/static/media/addItem.54a0c008.svg"
+  },
+  {
+    "revision": "be378556493b28158c75c9c1079ace87",
+    "url": "/static/media/NoRed.be378556.svg"
+  },
+  {
+    "revision": "2cc9646b52fb7e80645bc509e5e97110",
+    "url": "/static/media/carrot4.2cc9646b.png"
+  },
+  {
+    "revision": "8b7a60a98f9c1396a7b65b1c58fa2efb",
+    "url": "/static/media/RedProfile.8b7a60a9.svg"
+  },
+  {
+    "revision": "f463528d8bbce993676413806046d7f3",
+    "url": "/static/media/BlueProfile.f463528d.svg"
+  },
+  {
+    "revision": "eaa7cf0978c653db5ea57b78b68ae7f5",
+    "url": "/static/media/YellowProfile.eaa7cf09.svg"
+  },
+  {
+    "revision": "c7cca17548121153dd6adcf16f8c4405",
+    "url": "/static/media/removeFromCart.c7cca175.svg"
+  },
+  {
+    "revision": "714f99446a56ee1e8214b7bdc3636aa6",
+    "url": "/index.html"
+  }
+];
